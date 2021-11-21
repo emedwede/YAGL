93ae41e426713e8336d6570d8ee12f682eb18f78
Eric Medwedeff <emedwede@uci.edu>
2021-11-20 16:17:32 -0800

    Added the node class

---
+++ include/YAGL_Node.hpp
@@ -0,0 +1,72 @@
+#ifndef YAGL_NODE_H
+#define YAGL_NODE_H
+
+#pragma once 
+
+#include <iostream>
+
+namespace YAGL
+{
+	template <typename KeyType, typename DataType>
+	class Node;
+
+	template <typename KeyType, typename DataType>
+	std::ostream &operator<<(std::ostream& os, const Node<KeyType, DataType>& node);
+
+	template <typename KeyType, typename DataType>
+	class Node
+	{
+		public:
+			using data_type = DataType;
+			using key_type = KeyType;
+			
+			Node(const KeyType, const DataType& data);
+			~Node() = default;
+
+			const KeyType& getKey() const;
+			const DataType& getData() const;
+
+			bool operator==(const Node<KeyType, DataType>& b) const;
+			bool operator<(const Node<KeyType, DataType>& b) const;
+
+			friend std::ostream &operator<<<>(std::ostream &os, const Node<KeyType, DataType>& node);
+
+		private:
+			KeyType key;
+			DataType data;	
+	};
+
+	template <typename KeyType, typename DataType>
+	Node<KeyType, DataType>::Node(const KeyType, const DataType& data)
+	{
+		
+	}
+
+	template <typename KeyType, typename DataType>
+	const KeyType& Node<KeyType, DataType>::getKey() const
+	{
+		return key;
+	}
+
+	template <typename KeyType, typename DataType>
+	const DataType& Node<KeyType, DataType>::getData() const
+	{
+		return data;
+	}
+
+	template <typename KeyType, typename DataType>
+	bool Node<KeyType, DataType>::operator==(const Node<KeyType, DataType>& b) const
+	{
+		return (this->key == b.key && this-> data == b.data);
+	}
+	
+	template <typename KeyType, typename DataType>
+	std::ostream &operator<<(std::ostream &os, const Node<KeyType, DataType>& node)
+	{
+		os << "Node: {\n"
+			 << "\tKey:\t" << node.key << "\n"
+			 << "\tData:\t" << node.data << "\n";
+		return os;
+	}
+
+} //end namespace YAGL
