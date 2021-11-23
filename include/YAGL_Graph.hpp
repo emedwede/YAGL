c183c40d534ec0c021be5bf12f909c6f8231e723
Eric Medwedeff <emedwede@uci.edu>
2021-11-22 16:42:08 -0800

    Added graph types

--- include/YAGL_Graph.hpp
+++ include/YAGL_Graph.hpp
@@ -3,7 +3,10 @@
 
 #pragma once 
 
-#include <iostream>
+#include <iostream> 
+#include <unordered_map> 
+#include <unordered_set>
+#include <utility> // for pair 
 
 #include "YAGL_Node.hpp"
 
@@ -20,13 +23,41 @@ namespace YAGL
 	template <typename KeyType, typename DataType>
 	class Graph
 	{
+		// define types and iterators publicly
+		public:
+			using key_type = KeyType;
+			using data_type = DataType;
+			using node_type = Node<key_type, data_type>;
+			using edge_type = char; // TODO: remove placeholder
+			
+			// Motivation: unordered_set allows for amortized time
+			// 						 constant looup, and ensures keys are unique
+			//
+			// NOTE: 			 key_types should be hashable and comparable
+			// 						 and provide this if not available by default
+			using node_set_type = std::unordered_set<key_type>;
+			using edge_set_type = std::unordered_set<key_type>;
+			
+			// Motivation: easy way to find what goes in and what comes out 
+			using in_out_nbr_type = std::pair<node_set_type, node_set_type>;
+			
+			// Motivation: simliar to the node and edge_set types
+			using adjaceny_list_type = std::unordered_map<key_type, in_out_nbr_type>;
+			using node_list_type = std::unordered_map<key_type, node_type>;
+
+			// by default count with the largest unsigned type
+			using counting_type = std::size_t;
+
+			//TODO: define any useful iterators
+			
 		private:
+			node_list_type node_list;
+			adjaceny_list_type adjaceny_list;	
 
 		public:
-			Graph() = default;
+			Graph();
 
-			Graph( const DataType placeholder);
-			
+			Graph(const DataType placeholder);
 			
 			void setEdgeset(/* Needs to take in an edge set*/);
 			
@@ -48,17 +79,27 @@ namespace YAGL
 			
 			void removeNode();
 			
+			void numNodes();
+
+			void numEdges();
+
 			void isDirected();
 
 			void isUndirected();
 			
 			friend std::ostream &operator<<<>(std::ostream& os, const Graph<KeyType, DataType>& graph);
 	};
+	
+	template <typename KeyType, typename DataType>
+	Graph<KeyType, DataType>::Graph()
+	{
+		std::cout << "Default graph constructor!\n";
+	}
 
 	template <typename KeyType, typename DataType>
 	Graph<KeyType, DataType>::Graph(const DataType placeholder)
 	{
-
+		std::cout << "Overloaded graph const!\n";
 	}
 
 	template <typename KeyType, typename DataType>
@@ -117,6 +158,18 @@ namespace YAGL
 
 	template <typename KeyType, typename DataType>
 	void Graph<KeyType, DataType>::removeNode()
+	{
+
+	}
+
+	template <typename KeyType, typename DataType>
+	void Graph<KeyType, DataType>::numNodes()
+	{
+
+	}
+
+	template <typename KeyType, typename DataType>
+	void Graph<KeyType, DataType>::numEdges()
 	{
 
 	}
